c9923ac5bc8e151630b77bdf71243cbb3daee4f6
weixb <weixb@yaocaimaimai.com>
2016-08-17 11:25:45 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import configRouter from './router'
+require('./assets/style.css')
+
+Vue.use(VueRouter)
+const router = new VueRouter()
+configRouter(router)//注入路由规则
+
+router.start(Vue.extend(App),'#app')
+/* eslint-disable no-new */
