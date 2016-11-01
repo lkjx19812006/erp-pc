3693b7defdd29ae108edf02759af514f5b083379
huili.sun <sunhl@yaocaimaimai.com>
2016-11-01 18:02:35 +0800

    fixed

--- master/src/components/supply/createFiles.vue
+++ master/src/components/supply/createFiles.vue
@@ -45,9 +45,9 @@
                     </div>
                     <div class="editpage-input">
                         <label class="editlabel">新建文件</label>
-                        <press-image :value.sync="param.image_f" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:30%"></press-image>
-                       <press-image :value.sync="param.image_s" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:30%"></press-image>
-                       <press-image :value.sync="param.image_t" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:30%"></press-image>
+                        <press-image :value.sync="param.image_f" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:20%"></press-image>
+                       <press-image :value.sync="param.image_s" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:20%"></press-image>
+                       <press-image :value.sync="param.image_t" :type="type" :param="imageParam" style="float:left;margin-left:15px;width:20%"></press-image>
                     </div>
                 </section>
             </div>
@@ -140,29 +140,15 @@ export default {
 }
 </script>
 <style scoped>
-.big-font {
-    font-size: 36px;
-}
-
-.top-title span {
-    font-size: 28px;
+.top-title{
+    position: absolute;
+    top: 0;
+    right: 0;
 }
 .edit-content {
     padding: 19px 10px;
     text-align: center;
     border-bottom: 1px solid #ddd;
-}
-
-.edit-content h3 {
-    font-size: 20px;
-    color: #fa6705;
-    margin: 0;
-}
-
-.edit-model {
-    overflow: hidden;
-    overflow-y: auto;
-    padding: 10px 30px 30px 30px;
 }
 
 .editsection {
@@ -175,11 +161,6 @@ export default {
 }
 .editpage-input {
     margin-top: 15px;
-}
-.editlabel {
-    color: #333;
-    font-size: 14px;
-    display: block;
 }
 
 .edit-input {
