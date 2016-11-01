cc5f655829fefdf1b437b3be676d3938b16a56ab
linqi <linq@yaocaimaimai.com>
2016-11-01 19:48:35 +0800

    fix

--- master/src/components/tips/tipDialog.vue
+++ master/src/components/tips/tipDialog.vue
@@ -30,10 +30,10 @@ export default {
 <style scoped>
 
 .del_modal_con{
-    z-index:1084;
+    z-index:1091;
 }
 .modal {
-    z-index:1083;
+    z-index:1090;
     opacity: 0.5;
     background-color: #000;
     display: block;
