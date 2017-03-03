fca94cddc40851927cfda4c335499395325989b9
huili.sun <sunhl@yaocaimaimai.com>
2017-03-03 09:41:10 +0800

    order

--- master/src/vuex/actions.js
+++ master/src/vuex/actions.js
@@ -1243,8 +1243,7 @@ export const alterOrder = ({ dispatch }, param) => { //修改订单
         customerName: param.customerName,
         consigneeAddr: param.consigneeAddr,
         comments: param.comments,
-        orderStatus: param.orderStatus
-,
+        orderStatus: param.orderStatus,
         goods: param.goods
     }
     if (param.consigner) {
