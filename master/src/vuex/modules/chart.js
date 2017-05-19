0c6befd42e762bfe0e8134ea784bf47affea2dd1
caijian <825388643@qq.com>
2017-05-19 15:11:09 +0800

    fix

--- master/src/vuex/modules/chart.js
+++ master/src/vuex/modules/chart.js
@@ -1873,7 +1873,7 @@ const state = {
                                '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
                             ]; 
                             if(params.dataIndex>colorList.length){
-                                return colorList[params.dataIndex-colorList.length]
+                                return colorList[params.dataIndex%15]
                             }                            
                             return colorList[params.dataIndex]
                         }
@@ -1936,7 +1936,7 @@ const state = {
                                '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
                             ]; 
                             if(params.dataIndex>colorList.length){
-                                return colorList[params.dataIndex-colorList.length]
+                                return colorList[params.dataIndex%15]
                             }                            
                             return colorList[params.dataIndex]
                         }
@@ -1997,10 +1997,10 @@ const state = {
                                 '#C1232B','#B5C334','#FCCE10','#E87C25','#27727B',
                                '#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD',
                                '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
-                            ]; 
+                            ];
                             if(params.dataIndex>colorList.length){
-                                return colorList[params.dataIndex-colorList.length]
-                            }                            
+                                return colorList[params.dataIndex%15]
+                            }                          
                             return colorList[params.dataIndex]
                         }
                     }
